f0ce3dd950c2a71eedc3ec200127dd4ca6b561cd
FLP013 <felliperibeiro9@gmail.com>
2024-10-12 17:38:49 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+ 
+int Idade;
+
+Console.WriteLine("--- VOCÊ PODE SER DOADOR DE SANGUE? --- ");
+ 
+Console.Write("Digite sua idade: ");
+Idade = Convert.ToInt32(Console.ReadLine());
+ 
+if(Idade >= 18 && Idade <= 67)
+{
+Console.WriteLine("Você pode ser doador de sangue");
+ 
+}
+else
+{
+    Console.WriteLine("Você não pode ser doador de sangue");
+}
+ 
